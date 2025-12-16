386f36aa875dc059321f5a849328f307a2c39be5
RomanRoland <149831628+RomanRoland@users.noreply.github.com>
2025-12-16 16:08:26 +0200

    Add files via upload

---
+++ Grafica lab 9/Grafica lab 9/Form1.cs
@@ -0,0 +1,117 @@
+﻿namespace Grafica_lab_9
+{
+    public partial class Form1 : Form
+    {
+        TextBox txtAx, txtAy, txtAz;
+        TextBox txtBx, txtBy, txtBz;
+        TextBox txtCx, txtCy, txtCz;
+        Button btnCompute;
+        TextBox txtNormal;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Normală triunghi 3D";
+            this.ClientSize = new Size(500, 400);
+            this.DoubleBuffered = true;
+            CreateUI();
+        }
+        private void CreateUI()
+        {
+            int y = 10;
+            Label lblA = new Label() { Text = "A (x,y,z):", Location = new Point(10, y), AutoSize = true };
+            txtAx = new TextBox() { Location = new Point(80, y), Width = 50, Text = "0" };
+            txtAy = new TextBox() { Location = new Point(140, y), Width = 50, Text = "0" };
+            txtAz = new TextBox() { Location = new Point(200, y), Width = 50, Text = "0" };
+            this.Controls.Add(lblA); this.Controls.Add(txtAx); this.Controls.Add(txtAy); this.Controls.Add(txtAz);
+            y += 30;
+            Label lblB = new Label() { Text = "B (x,y,z):", Location = new Point(10, y), AutoSize = true };
+            txtBx = new TextBox() { Location = new Point(80, y), Width = 50, Text = "1" };
+            txtBy = new TextBox() { Location = new Point(140, y), Width = 50, Text = "0" };
+            txtBz = new TextBox() { Location = new Point(200, y), Width = 50, Text = "0" };
+            this.Controls.Add(lblB); this.Controls.Add(txtBx); this.Controls.Add(txtBy); this.Controls.Add(txtBz);
+            y += 30;
+            Label lblC = new Label() { Text = "C (x,y,z):", Location = new Point(10, y), AutoSize = true };
+            txtCx = new TextBox() { Location = new Point(80, y), Width = 50, Text = "0" };
+            txtCy = new TextBox() { Location = new Point(140, y), Width = 50, Text = "1" };
+            txtCz = new TextBox() { Location = new Point(200, y), Width = 50, Text = "0" };
+            this.Controls.Add(lblC); this.Controls.Add(txtCx); this.Controls.Add(txtCy); this.Controls.Add(txtCz);
+            y += 40;
+            btnCompute = new Button() { Text = "Calculează normală", Location = new Point(10, y), Width = 200 };
+            btnCompute.Click += BtnCompute_Click;
+            this.Controls.Add(btnCompute);
+            y += 40;
+            txtNormal = new TextBox() { Location = new Point(10, y), Width = 300, ReadOnly = true };
+            this.Controls.Add(txtNormal);
+        }
+
+        private void BtnCompute_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Vector3 A = new Vector3(
+                    double.Parse(txtAx.Text),
+                    double.Parse(txtAy.Text),
+                    double.Parse(txtAz.Text)
+                );
+                Vector3 B = new Vector3(
+                    double.Parse(txtBx.Text),
+                    double.Parse(txtBy.Text),
+                    double.Parse(txtBz.Text)
+                );
+                Vector3 C = new Vector3(
+                    double.Parse(txtCx.Text),
+                    double.Parse(txtCy.Text),
+                    double.Parse(txtCz.Text)
+                );
+                Vector3 normal = ComputeNormal(A, B, C);
+                txtNormal.Text = $"Normală unitate: {normal}";
+            }
+            catch
+            {
+                MessageBox.Show("Introduceți valori valide numerice!");
+            }
+        }
+        public struct Vector3
+        {
+            public double X, Y, Z;
+            public Vector3(double x, double y, double z)
+            {
+                X = x; Y = y; Z = z;
+            }
+            public static Vector3 operator -(Vector3 a, Vector3 b)
+            {
+                return new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+            }
+            public static Vector3 Cross(Vector3 a, Vector3 b)
+            {
+                return new Vector3(
+                    a.Y * b.Z - a.Z * b.Y,
+                    a.Z * b.X - a.X * b.Z,
+                    a.X * b.Y - a.Y * b.X
+                );
+            }
+            public double Length()
+            {
+                return Math.Sqrt(X * X + Y * Y + Z * Z);
+            }
+            public Vector3 Normalize()
+            {
+                double len = Length();
+                if (len == 0) return new Vector3(0, 0, 0);
+                return new Vector3(X / len, Y / len, Z / len);
+            }
+            public override string ToString()
+            {
+                return $"({X:F3}, {Y:F3}, {Z:F3})";
+            }
+        }
+
+        public static Vector3 ComputeNormal(Vector3 A, Vector3 B, Vector3 C)
+        {
+            Vector3 U = B - A;
+            Vector3 V = C - A;
+            Vector3 N = Vector3.Cross(U, V);
+            return N.Normalize();
+        }
+    }
+}
---
+++ grafica lab5/grafica lab5/FereastraClipping.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grafica_lab5
+{
+    public class FereastraClipping : Form
+    {
+        const int INTERIOR = 0;
+        const int STANGA = 1;
+        const int DREAPTA = 2;
+        const int JOS = 4;
+        const int SUS = 8;
+        float xmin = 100, ymin = 100, xmax = 300, ymax = 200;
+        public FereastraClipping()
+        {
+            this.Text = "Cohen-Sutherland";
+            this.Width = 600;
+            this.Height = 450;
+            this.Paint += Desenare;
+        }
+        int CodRegiune(float x, float y)
+        {
+            int cod = INTERIOR;
+            if (x < xmin) cod |= STANGA;
+            else if (x > xmax) cod |= DREAPTA;
+            if (y < ymin) cod |= JOS;
+            else if (y > ymax) cod |= SUS;
+            return cod;
+        }
+        void Clipping(Graphics g, float x1, float y1, float x2, float y2)
+        {
+            int cod1 = CodRegiune(x1, y1);
+            int cod2 = CodRegiune(x2, y2);
+            bool acceptata = false;
+
+            while (true)
+            {
+                if ((cod1 | cod2) == 0)
+                {
+                    acceptata = true;
+                    break;
+                }
+                else if ((cod1 & cod2) != 0)
+                {
+                    break;
+                }
+                else
+                {
+                    int codExterior = (cod1 != 0) ? cod1 : cod2;
+                    float x = 0, y = 0;
+
+                    if ((codExterior & SUS) != 0)
+                    {
+                        x = x1 + (x2 - x1) * (ymax - y1) / (y2 - y1);
+                        y = ymax;
+                    }
+                    else if ((codExterior & JOS) != 0)
+                    {
+                        x = x1 + (x2 - x1) * (ymin - y1) / (y2 - y1);
+                        y = ymin;
+                    }
+                    else if ((codExterior & DREAPTA) != 0)
+                    {
+                        y = y1 + (y2 - y1) * (xmax - x1) / (x2 - x1);
+                        x = xmax;
+                    }
+                    else if ((codExterior & STANGA) != 0)
+                    {
+                        y = y1 + (y2 - y1) * (xmin - x1) / (x2 - x1);
+                        x = xmin;
+                    }
+
+                    if (codExterior == cod1)
+                    {
+                        x1 = x; y1 = y; cod1 = CodRegiune(x1, y1);
+                    }
+                    else
+                    {
+                        x2 = x; y2 = y; cod2 = CodRegiune(x2, y2);
+                    }
+                }
+            }
+
+            if (acceptata)
+            {
+                Pen p = new Pen(Color.Red, 2);
+                g.DrawLine(p, x1, y1, x2, y2);
+            }
+        }
+        void Desenare(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            g.DrawRectangle(new Pen(Color.Black, 2), xmin, ymin, xmax - xmin, ymax - ymin);
+            g.DrawLine(new Pen(Color.Green, 1), 50, 50, 400, 250);
+
+            Clipping(g, 50, 50, 400, 250);
+        }
+        [STAThread]
+        public static void Main()
+        {
+            Application.Run(new FereastraClipping());
+        }
+    }
+}
